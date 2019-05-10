ff2f884b7f43b552fd61830fd9020394a001b933
BustosCecilia <ceciliabus@gmail.com>
2019-05-10 13:00:25 -0300

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,156 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import domain.Item;
+import org.apache.http.HttpHost;
+import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class Application {
+
+    //The config parameters for the connection
+    private static final String HOST = "localhost";
+    private static final int PORT_ONE = 9200;
+    private static final int PORT_TWO = 9201;
+    private static final String SCHEME = "http";
+
+    private static RestHighLevelClient restHighLevelClient;
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final String INDEX = "itemdata";
+    private static final String TYPE = "item";
+
+
+    /**
+     * Implemented Singleton pattern here
+     * so that there is just one connection at a time.
+     * @return RestHighLevelClient
+     */
+    private static synchronized RestHighLevelClient makeConnection() {
+
+        if(restHighLevelClient == null) {
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(
+                            new HttpHost(HOST, PORT_ONE, SCHEME),
+                            new HttpHost(HOST, PORT_TWO, SCHEME)));
+        }
+
+        return restHighLevelClient;
+    }
+
+    private static synchronized void closeConnection() throws IOException {
+        restHighLevelClient.close();
+        restHighLevelClient = null;
+    }
+
+    private static Item insertItem(Item item){
+        Map<String, Object> dataMap = new HashMap<String, Object>();
+        dataMap.put("id", item.getId());
+        dataMap.put("siteId", item.getSiteId());
+        dataMap.put("title", item.getTitle());
+        dataMap.put("subtitle", item.getSubtitle());
+        dataMap.put("sellerId", item.getSellerId());
+        dataMap.put("categoryId", item.getCategoryId());
+
+        IndexRequest indexRequest = new IndexRequest(INDEX, TYPE, item.getId())
+                .source(dataMap);
+        try {
+            IndexResponse response = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+        } catch(ElasticsearchException e) {
+            e.getDetailedMessage();
+        } catch (IOException ex){
+            ex.getLocalizedMessage();
+        }
+        return item;
+    }
+
+
+    private static Item getItemById(String id){
+        GetRequest getPersonRequest = new GetRequest(INDEX, TYPE, id);
+        GetResponse getResponse = null;
+        try {
+            getResponse = restHighLevelClient.get(getPersonRequest, RequestOptions.DEFAULT);
+        } catch (IOException e){
+            e.getLocalizedMessage();
+        }
+        return getResponse != null ?
+                objectMapper.convertValue(getResponse.getSourceAsMap(), Item.class) : null;
+    }
+
+
+    private static Item updateItemById(String id, Item item){
+        UpdateRequest updateRequest = new UpdateRequest(INDEX, TYPE, id)
+                .fetchSource(true);    // Fetch Object after its update
+        try {
+            String itemJson = objectMapper.writeValueAsString(item);
+            updateRequest.doc(itemJson, XContentType.JSON);
+            UpdateResponse updateResponse = restHighLevelClient.update(updateRequest, RequestOptions.DEFAULT);
+            return objectMapper.convertValue(updateResponse.getGetResult().sourceAsMap(), Item.class);
+        }catch (JsonProcessingException e){
+            e.getMessage();
+        } catch (IOException e){
+            e.getLocalizedMessage();
+        }
+        System.out.println("Unable to update item");
+        return null;
+    }
+
+    private static void deleteItemById(String id) {
+        DeleteRequest deleteRequest = new DeleteRequest(INDEX, TYPE, id);
+        try {
+            DeleteResponse deleteResponse = restHighLevelClient.delete(deleteRequest, RequestOptions.DEFAULT);
+        } catch (IOException e){
+            e.getLocalizedMessage();
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
+        makeConnection();
+
+        System.out.println("Inserto un nuevo item ...");
+        Item item = new Item();
+        item.setId("MLA608007087");
+        item.setSiteId("MLA");
+        item.setTitle("Item De Testeo, Por Favor No Ofertar --kc:off");
+        item.setSubtitle("testeo");
+        item.setSellerId(202593498);
+        item.setCategoryId("MLA3530");
+        item = insertItem(item);
+        System.out.println("item inserted --> " + item.toString());
+
+        System.out.println("Inserto un otro  item ...");
+        Item item1 = new Item();
+        item1.setId("MLA608001111");
+        item1.setSiteId("MLB");
+        item1.setTitle("Item De Testeo, Por Favor No Ofertar --kc:off");
+        item1.setSubtitle("testeo");
+        item1.setSellerId(202593498);
+        item1.setCategoryId("MLA3530");
+        item1 = insertItem(item1);
+        System.out.println("item inserted --> " + item1.toString());
+
+        System.out.println("Changing site_Id to `MLC`...");
+        item.setSiteId("MLC");
+        updateItemById(item.getId(), item);
+        System.out.println("Item updated  --> " + item);
+
+        closeConnection();
+    }
+}
